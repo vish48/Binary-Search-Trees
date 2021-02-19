3efd0fc7911d584c2de0153dad1a3294443f4c89
Nicholas <nicosmith.smith3@gmail.com>
2021-02-19 12:29:11 -0400

    Fixed Spelling error in comments

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -1,14 +1,13 @@
 #ifndef _BINARYSEARCHTREE_H
 #define _BINARYSEARCHTREE_H
 
-// function prototypes for functions given
+// function prototypes
 
 BTNode * recTreeSearch (BTNode * root, string key);
 BTNode * treeMinimum (BTNode * root);
 BTNode * treeMaximum (BTNode * root);
 BTNode * inOrderSuccessor (BTNode * node);
 
-// function prototypes for Assignment 2
 
 BTNode * ceiling (BTNode * root, string key);
 BTNode * deleteNode (BTNode * root, string key);

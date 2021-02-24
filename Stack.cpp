7eb4f1068bf722b9482dfb8f95f44ac041165a36
Nicholas <nicosmith.smith3@gmail.com>
2021-02-24 19:08:21 -0400

    Added comments to methods

--- Stack.cpp
+++ Stack.cpp
@@ -5,14 +5,17 @@
 
 using namespace std;
 
-
+//Returns a pointer to a Stack Object
+//Instantiates a Stack pointer variable s
 Stack * initStack () {
 	Stack * s = new Stack;
-	s->top = NULL;
+	s->top = NULL; //Sets top of the Stack to NULL
 	return s;
 }
 
-
+//Returns either true or false
+//Parameters: Stack * s
+//if top of stack is pointing to a NULL value then it is empty
 bool isEmptyStack (Stack * s) {
 	if (s->top == NULL)
 		return true;
@@ -20,7 +23,9 @@ bool isEmptyStack (Stack * s) {
 		return false;		
 }
 
-
+//Parameters: Stack * s, BTNode * data
+//Creates a new Node variable to store data.
+//Assigns the top of the stack to that variable 
 void push (Stack * s, BTNode * data) {
 	Node * newElement = new Node;
 	newElement->data = data;
@@ -28,7 +33,7 @@ void push (Stack * s, BTNode * data) {
 	s->top = newElement;
 }
 
-
+//Returns the value at the top of the stack
 BTNode * pop (Stack * s) {
 	if (isEmptyStack(s))
 		return NULL; 				// to indicate error if popping from an empty stack
